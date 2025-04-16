3b617a665ab84de509b3dc65d132c161f2c26da3
Dmitry Yampolsky <79473397+DYPortfolio@users.noreply.github.com>
2025-04-16 16:44:44 -0500

    Add files via upload

---
+++ Lattractor/Lattractor.hpp
@@ -0,0 +1,15 @@
+//
+//  Lattractor.hpp
+//  opcltest
+//
+//  Created by Khan on 8/12/22.
+//
+
+#ifndef Lattractor_hpp
+#define Lattractor_hpp
+
+#include <stdio.h>
+
+void lattractor();
+
+#endif /* Lattractor_hpp */
